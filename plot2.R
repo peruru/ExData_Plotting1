413ea3341cb3bb829ca45f5e49cf4e6aabe79be3
peruru <suneel.peruru@gmail.com>
2015-08-07 22:13:28 +0530

    Updated R files for Plots

--- plot2.R
+++ plot2.R
@@ -1,6 +1,5 @@
 install.packages("sqldf")
 library(sqldf)
-setwd("D:/LDP/00 - Updated-Folder/Personal/Big Data/Coursera - Explorartory")
 
 df = read.csv.sql("household_power_consumption.txt", "select * from file where Date in ('1/2/2007', '2/2/2007')",header = TRUE, sep = ";")
 summary(df)
